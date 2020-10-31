1383a07661f6bb197daef6285d5cccc1f1bcf141
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-31 15:48:02 +0000

    directors/[DIRECTOR ID]

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -8,4 +8,11 @@ class DirectorsController < ApplicationController
     @oldest = Director.where.not({:dob => nil}).order({:dob => :asc}).at(0)
     render({ :template => "director_templates/eldest.html.erb" })
   end
+
+  def director_details
+    # params looks like {"an_id" => "42"}
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({ :id => the_id }).at(0)
+    render({ :template => "director_templates/show.html.erb"})
+  end
 end
