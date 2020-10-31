56d7a12e9a8f3e93ac757b01c1239d4d65d98bbd
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-31 16:06:40 +0000

    finished following along the video

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,6 +13,7 @@ class DirectorsController < ApplicationController
     # params looks like {"an_id" => "42"}
     the_id = params.fetch("an_id")
     @the_director = Director.where({ :id => the_id }).at(0)
+    @filmography = Movie.where({ :director_id => @the_director.id })
     render({ :template => "director_templates/show.html.erb"})
   end
 end
